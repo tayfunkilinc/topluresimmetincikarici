3cd8a806fb4a59cb13b2ea3da54609c32a6a3711
Tayfun KILINÇ <153390338+tayfunkilinc@users.noreply.github.com>
2025-12-05 00:33:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Cloudflare Pages alt dizin için düzgün base path ayarı
 export default defineConfig({
   base: '/topluresimcevirici/',
+
   plugins: [react()],
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
