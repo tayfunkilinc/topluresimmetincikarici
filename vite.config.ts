6cedc4b24f0b0f3541ba11a6b3d80a36cbe9da6c
Tayfun KILINÇ <153390338+tayfunkilinc@users.noreply.github.com>
2025-12-05 00:28:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/topluresimcevirici/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
